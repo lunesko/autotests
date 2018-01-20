cc64c3e9e997f5f50ed51f4258fb8e6ad6d82975
unknown <billiscorp@gmail.com>
2018-01-20 17:45:15 +0200

    auto

--- index.js
+++ index.js
@@ -6,5 +6,10 @@ express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .get('/', (req, res) => res.render('pages/index')) 
+  .get('/db', (req, res) => res.render('pages/db'))
+  .get('/test/.*$', (req, res) => {
+	res.send(req.path);
+	//res.render('pages/test')
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
