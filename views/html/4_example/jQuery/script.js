83a1feab6842279465a903059cd838ca6dd6699a
unknown <billiscorp@gmail.com>
2018-03-15 15:04:19 +0200

    add JQuery exampl

---
+++ views/html/4_example/jQuery/script.js
@@ -0,0 +1,37 @@
+$("h2").css("color","green");
+$("h2").css("color","red");
+
+// $(document).ready(function(){
+// 		$("h2").css("color","green");	
+// });
+
+
+
+// $(function(){
+// 		$("h2").css("color","red");
+// });
+
+
+// $(document).ready(function() {
+// 	 $("h2").html("Подменили текст");
+// })
+
+// jQuery(document).ready(function() {
+// 	 jQuery("h2").html("Подменили текст");
+// })
+
+// $("h2").html("Подменили текст");
+
+// $('#btn_start').click(function(){
+// 		$('#message')
+// 		.html("New Message")
+// 		.css("background-color","red")
+// 		.parent()
+// 		.css("background-color","green")
+// 		.width(150)
+// 		.height(80)
+// });
+
+// // $('#btn_reset').click(function(){
+// // 	location.reload();
+// });
