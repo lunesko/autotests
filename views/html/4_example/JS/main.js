8d9f70dd8e20dd057848b0ca21d1250ae1789177
billiscorp <billiscorp@gmail.com>
2018-08-04 13:38:56 +0300

    add JS example

---
+++ views/html/4_example/JS/main.js
@@ -0,0 +1,19 @@
+ //window.open('https://google.com');
+
+/*document.body.style.background = 'red';
+console.log( 'Элемент BODY стал красным, а сейчас обратно вернётся' );
+ document.body.style.background = '';*/
+
+alert( location.href );
+
+// console.log(navigator);
+/*console.log(navigator.userAgent);
+console.log(navigator.platform);
+
+console.log(screen);
+console.log(screen.width + 'x' + screen.height);
+
+console.log(history);*/
+
+
+
